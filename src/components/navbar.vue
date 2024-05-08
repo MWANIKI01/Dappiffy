916c972623557544d149ca667d64a1de87ab815b
Kaberere mwaniki <mwanikikaberere16@gmail.com~>
2024-05-08 10:53:27 +0000

    Updated README.md with project information

---
+++ src/components/navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  
+  
+</template>
+
+<script>
+export default {
+  name: 'navbar',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
